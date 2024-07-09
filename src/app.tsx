1fac5bf5b8a608aeb86dce15be386ac8c1810661
dalramjwi <dalramjwi@gmail.com>
2024-07-09 12:28:01 +0900

    arr map use but err

--- src/app.tsx
+++ src/app.tsx
@@ -3,12 +3,17 @@ import Minam from "./minam";
 import basicData from "./static/basic-data";
 import Ul from "./ul";
 import Li from "./li";
+import arr from "./static/array-data";
 const App: React.FC = () => {
   return (
     <div>
       <Minam textNode={basicData} />
       <h1>Hello</h1>
-      <Ul textNode={<Li textNode={basicData} />} />
+      <Ul>
+        {arr.map((item, index) => (
+          <Li key={index} textNode={item} />
+        ))}
+      </Ul>
     </div>
   );
 };
