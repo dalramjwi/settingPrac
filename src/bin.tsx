d0697abf05e7745b88a9c170c9bf06133f3c21e8
dalramjwi <dalramjwi@gmail.com>
2024-07-09 12:45:37 +0900

    styling

--- src/bin.tsx
+++ src/bin.tsx
@@ -3,9 +3,12 @@ interface BinProps {
   h1TextNode: string;
 }
 const Bin: React.FC<BinProps> = ({ h1TextNode }) => {
+  let style: React.CSSProperties = {
+    color: "blue",
+  };
   return (
     <div>
-      <h1>{h1TextNode}</h1>
+      <h1 style="color: red;">{h1TextNode}</h1>
     </div>
   );
 };
