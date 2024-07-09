44917d4cf01e4db15bb2a30f619fd17415b9698b
dalramjwi <dalramjwi@gmail.com>
2024-07-09 12:42:43 +0900

    array 변경

--- src/static/array-data.tsx
+++ src/static/array-data.tsx
@@ -1,11 +1,11 @@
 const arr: Array<string> = [
-  "one",
-  "two",
-  "three",
-  "four",
-  "five",
-  "six",
-  "seven",
-  "eight",
+  "칠리새우",
+  "크림새우",
+  "국물닭발",
+  "찹쌀탕수육",
+  "순대국밥",
+  "크레페",
+  "브이콘",
+  "치즈폼흑당스무디",
 ];
 export default arr;
